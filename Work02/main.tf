3f580af5cbb1db9004cb7fbc2edf7baaaa056313
Bhuiyan Muhaimeen <bmuhaime@versa-networks.com>
2022-05-27 10:30:49 -0700

    Second Commit

---
+++ Work02/main.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_instance" "TF_WORK02" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+
+  user_data = <<-EOF
+	             #!/bin/bash
+	             echo "Hello, World" > index.html
+	             nohup busybox httpd -f -p 8080 &
+	             EOF
+
+
+  tags = {
+    Name = "terraform-example"
+  }
+
+}
